2ced06b53c765783f43fc057ca83643e714099e3
Zephyssss <ngoc861999@gmail.com>
2021-01-04 22:09:14 +0700

    hibernate base interface DAO, database

---
+++ CustomHibernateSQL/DAO/BaseDAO.cs
@@ -0,0 +1,33 @@
+﻿using CustomHibernateSQL.AbstractDB;
+using CustomHibernateSQL.SQLServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CustomHibernateSQL.DAO
+{
+    public abstract class BaseDAO<T>
+    {
+        private IDatabase db = new SQLDatabase();
+        private Type type = typeof(T);
+        public List<T> GetAll()
+        {
+            return db.GetAll(type).Cast<T>().ToList();
+        }
+
+        public int Update(T obj)
+        {
+            return db.Update(obj);
+        }
+
+        public int Delete(T obj)
+        {
+            return db.Delete(obj);
+        }
+
+        public int Insert(T obj)
+        {
+            return db.Insert(obj);
+        }
+    }
+}
---
+++ CustomHibernateSQL/SQLServer/SQLDatabase.cs
@@ -0,0 +1,33 @@
+﻿using CustomHibernateSQL.AbstractDB;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomHibernateSQL.SQLServer
+{
+    class SQLDatabase: IDatabase
+    {
+        public int Delete(object obj)
+        {
+            return 1;
+        }
+
+        public IList GetAll(Type type)
+        {
+            return new List<int>();
+        }
+
+        public int Insert(object obj)
+        {
+            return 1;
+        }
+
+        public int Update(object obj)
+        {
+            return 1;
+        }
+    }
+}
