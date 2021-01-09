b43337e14a3c7483a2f3c1bad840187acdba6fbb
Zephyssss <ngoc861999@gmail.com>
2021-01-09 17:01:18 +0700

    add config sql and executer Sql

---
+++ CustomHibernateSQL/SQLServer/SQLConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomHibernateSQL.SQLServer
+{
+    class SQLConfig
+    {
+        private static SQLConfig _instance = new SQLConfig();
+        public static SQLConfig Instance() => _instance;
+        private SQLConfig()
+        {
+
+        }
+
+        public string ConnectionString { get; set; } = "Data Source=DESKTOP-1RVEUQ2;Initial Catalog=Testing;User ID=nhinlee;Password=.";
+    }
+}
