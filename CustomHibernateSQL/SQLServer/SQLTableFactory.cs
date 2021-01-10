f15499a30e8f6c0efe13f4a5e28b717ea35a2800
Zephyssss <ngoc861999@gmail.com>
2021-01-10 11:54:20 +0700

    get Sql table from type T

---
+++ CustomHibernateSQL/SQLServer/SQLTableFactory.cs
@@ -0,0 +1,33 @@
+﻿using CustomHibernateSQL.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomHibernateSQL.SQLServer
+{
+    class SQLTableFactory
+    {
+        public static SQLTable BuildSQLTable(Type type)
+        {
+            // Get Table Attributes -> SQL Table object
+            TableAttribute tableAttr = (TableAttribute)type.GetCustomAttributes(typeof(TableAttribute), false)[0];
+            SQLTable table = new SQLTable(tableAttr.TableName, tableAttr.Schema, type);
+
+
+            // Get Field Attributes -> DB Field Name
+            var members = new List<PropertyInfo>();
+            members.AddRange(type.GetProperties());
+            foreach (var member in members)
+            {
+                ColumnAttribute columnAttr = (ColumnAttribute)member.GetCustomAttributes(typeof(ColumnAttribute), false)[0];
+                SQLColumn col = new SQLColumn(columnAttr.FieldName, member, columnAttr.Flag);
+                table.Columns.Add(col);
+            }
+
+            return table;
+        }
+    }
+}
